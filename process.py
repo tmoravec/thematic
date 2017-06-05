60be40faa7352d08620cfd03f0befb275bfcde15
Tadeas Moravec <moravec.tadeas@gmail.com>
2017-06-05 20:38:18 +0200

    Cleanup and small improvements.
    New distance metric for the summarization. Stopped removing the
    largest cluster. Removed "special" support for small pages: always use
    100 SVD dimensions.

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import copy
 import json
 import sys
 import pickle
+import math
 
 import matplotlib
 # Use a backend that doesn't require X server.
@@ -22,6 +23,7 @@ from nltk.tokenize.punkt import PunktSentenceTokenizer
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.neighbors import kneighbors_graph
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import calinski_harabaz_score
 from sklearn.decomposition import TruncatedSVD
@@ -29,6 +31,7 @@ from sklearn.preprocessing import Normalizer
 from sklearn.pipeline import make_pipeline
 from sklearn.manifold import TSNE
 from sklearn.metrics.pairwise import linear_kernel
+from sklearn.metrics.pairwise import pairwise_distances
 
 import gensim
 
@@ -54,7 +57,8 @@ def plot_clusters(features, labels, pagename):
     #pyplot.xlabel('x')
     #pyplot.ylabel('y', rotation='horizontal')
     pyplot.axis('off')
-    pyplot.savefig('{}/{}.png'.format(OUTPUT_DIRECTORY, pagename), format='png', dpi=300, bbox_inches='tight')
+    pyplot.savefig('{}/{}.png'.format(OUTPUT_DIRECTORY, pagename),
+                   format='png', dpi=300, bbox_inches='tight')
     #pyplot.clf()
 
 
@@ -130,19 +134,11 @@ def vectorize(messages):
         print(time.ctime(), len(messages), 'messages. Small page.')
         SMALL_PAGE = True
 
-    min_df_range = range(10, 1, -1)
-    if SMALL_PAGE:
-        min_df_range = range(3, 1, -1)
-
-    for i in min_df_range:
-        vectorizer = TfidfVectorizer(stop_words=stop_words, max_features=None,
-                                     ngram_range=(1, 5), norm='l2',
-                                     sublinear_tf=True, min_df=i, max_df=0.7)
-        features = vectorizer.fit_transform(messages)
-        if features.shape[1] > 200:
-            print(time.ctime(), 'min_df:', i)
-            break
-
+    vectorizer = TfidfVectorizer(stop_words=stop_words, max_features=None,
+                                 strip_accents=None, ngram_range=(1, 5),
+                                 norm='l2', sublinear_tf=True,
+                                 min_df=3, max_df=0.7)
+    features = vectorizer.fit_transform(messages)
     if features.shape[1] < 200:
         print(time.ctime(), features.shape[2], 'tfidf features. Small page.')
         SMALL_PAGE = True
@@ -200,7 +196,8 @@ def text_features(raw_data):
     features, vectorizer = vectorize(messages)
 
     # TODO: Wrap the return values to something like Features object...
-    return np.array(likes), np.array(comments), np.array(shares), features, texts, dates, vectorizer
+    return (np.array(likes), np.array(comments), np.array(shares),
+            features, texts, dates, vectorizer)
 
 
 def list_stats(L):
@@ -214,27 +211,15 @@ def list_stats(L):
     return mean, std, pct_rstd
 
 
-def highest_number_items(items, max_count=100):
-    """
-    From a nested list, with leaves formed of tuples (whatever, number),
-    selects up to max_count tuples with highest number, globally.
-    """
-    all = []
-    for i in items:
-        all += i
-    s = sorted(all, key=lambda x: x[1], reverse=True)
-    used = []
-    hnitems = [x[0] for x in s[:max_count] if x[0] not in used and (used.append(x[0]) or True)]
-    return hnitems[:10]
-
-
 def count_vectorize(corpus, count=10):
     stop_words = get_stopwords()
     messages = []
     for message in corpus:
         messages.append(process_message(message))
 
-    vectorizer = CountVectorizer(stop_words=stop_words, max_features=count, ngram_range=(1, 5), max_df=0.7)
+    vectorizer = CountVectorizer(stop_words=stop_words,
+                                 strip_accents=None, max_features=count,
+                                 ngram_range=(1, 5), max_df=0.7)
     features = vectorizer.fit_transform(messages)
     return vectorizer.get_feature_names()
 
@@ -245,20 +230,22 @@ def best_number_clusters(X):
     # would work better.
     scores = []
 
-    cluster_sizes = range(15, 31, 1)
+    cluster_sizes = range(15, 41, 1)
     if SMALL_PAGE:
         cluster_sizes = range(10, 20, 1)
 
     try:
         for n in cluster_sizes:
-            predictor = AgglomerativeClustering(n_clusters=n, connectivity=None, linkage='ward', affinity='euclidean')
+            knn_graph = kneighbors_graph(X, 20, include_self=False, n_jobs=-1)
+            predictor = AgglomerativeClustering(n_clusters=n,
+                                                connectivity=knn_graph,
+                                                linkage='ward',
+                                                affinity='euclidean')
             labels = predictor.fit_predict(X)
 
-            indices_to_keep = remove_noise_clusters(X, labels)
-            labelsI = [labels[i] for i in indices_to_keep]
-            XI = [X[i] for i in indices_to_keep]
-
-            scores.append(silhouette_score(XI, labelsI))
+            scores.append(silhouette_score(X, labels))
+            print(time.ctime(), '{} clusters: silhouette_score: {:.4f}'.format(
+                n, silhouette_score(X, labels)))
     except KeyboardInterrupt:
         pass
 
@@ -286,6 +273,20 @@ def clusterize(labels, likes, comments, shares, messages, tf, dates):
     return clusters
 
 
+def common_words_distance(a, b):
+    common_words_count = len(np.intersect1d(np.nonzero(a[0]), np.nonzero(b[0])))
+    lens = math.log(a.shape[1]) + math.log(b.shape[1])
+    if lens:
+        return common_words_count / lens
+    else:
+        return 0
+
+
+def common_words_similarity(X):
+    pass
+
+
+
 def summarize(messages):
     # Shouldn't we use the global tfidf/LSA values?
     # No. We'd need to operate on the whole posts, which
@@ -294,13 +295,18 @@ def summarize(messages):
     sentences = sentence_tokenizer.tokenize(' '.join(messages))
     sentences = set(sentences)
 
-    lower_sentences = [s.lower() for s in sentences]
-    tfidf = TfidfVectorizer().fit_transform(lower_sentences)
-    similarity_graph = linear_kernel(tfidf)
+    lower_sentences = [process_message(s) for s in sentences]
+    stop_words = get_stopwords()
+    bow = CountVectorizer(stop_words=stop_words, strip_accents=None,
+                          binary=True, ngram_range=(1, 5),
+                          min_df=3, max_df=0.7).fit_transform(lower_sentences)
+    similarity_graph = pairwise_distances(bow, metric=common_words_distance,
+                                          n_jobs=-1)
 
     nx_graph = nx.from_numpy_matrix(similarity_graph)
     scores = nx.pagerank_numpy(nx_graph)
-    most_characteristic = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+    most_characteristic = sorted(((scores[i], s) for i, s in enumerate(sentences)),
+                                 reverse=True)
 
     if len(most_characteristic) > 50:
         most_characteristic = most_characteristic[:50]
@@ -335,7 +341,8 @@ def keywords(text):
 
 
 def print_clusters(labels, likes, comments, shares, messages, tf, dates,
-                   vectorizer, orig_size, pagename, fan_count, page_name_displayed):
+                   vectorizer, orig_size, pagename, fan_count,
+                   page_name_displayed):
 
     # Break messages by sentences.
     # Perform LSA on them.
@@ -356,10 +363,12 @@ def print_clusters(labels, likes, comments, shares, messages, tf, dates,
 
 
     clusters = clusterize(labels, likes, comments, shares, messages, tf, dates)
-    clusters = sorted(clusters, key=lambda c: int(np.mean(c['shares'])), reverse=True)
+    clusters = sorted(clusters, key=lambda c: int(np.mean(c['shares'])),
+                      reverse=True)
 
     clusters_print = []
     for i, c in enumerate(clusters):
+        print(time.ctime(), 'Printing cluster {}'.format(i))
         likes_avg = int(np.mean(c['likes']))
         likes_stdev = int(np.std(c['likes']))
         comments_avg = int(np.mean(c['comments']))
@@ -405,7 +414,7 @@ def print_clusters(labels, likes, comments, shares, messages, tf, dates,
 
 
 def get_features_lsa(tf):
-    svd_components = 200
+    svd_components = 100
     if SMALL_PAGE:
         svd_components = 100
 
@@ -429,65 +438,26 @@ def find_largest_cluster(labels):
     return largest_cluster_index
 
 
-def remove_noise_clusters(X, labels):
-    # Print scores while removing clusters for start.
-    # Return indices to keep.
-    labelsI = copy.deepcopy(labels)
-    last_indices = range(len(labels))
-    last_score = silhouette_score(X, labels)
-
-    i = 0
-    while len(labelsI):
-        largest_cluster_index = find_largest_cluster(labelsI)
-        indices_to_keep = np.where(labelsI != largest_cluster_index)[0]
-        XI = np.array([X[x] for x in indices_to_keep])
-        labelsI = np.array([labels[x] for x in indices_to_keep])
-
-        #score_ch = calinski_harabaz_score(XI, labelsI)
-        score_sil = silhouette_score(XI, labelsI)
-
-        if score_sil < last_score:
-            break
-
-        print(time.ctime(), 'Last score: {:.4f}, removed cluster {}, current score: {:.4f}'.format(last_score, i, score_sil))
-        last_score = score_sil
-        last_indices = indices_to_keep
-
-
-        i += 1
-
-    return last_indices
-
-
 def text_clustering(raw_data, pagename):
     fan_count = raw_data['fan_count']
     page_name_displayed = raw_data['name']
     likes, comments, shares, tf, messages, dates, vectorizer = text_features(raw_data)
     orig_size = len(messages)
-
     X = get_features_lsa(tf)
 
     print(time.ctime(), 'Trying to determine best number of clusters.')
     n_clusters = best_number_clusters(X)
     print(time.ctime(), 'Best number of clusters: {}'.format(n_clusters))
 
-    predictor = AgglomerativeClustering(n_clusters=n_clusters, connectivity=None, linkage='ward', affinity='euclidean')
-    #predictor = KMeans(n_clusters=n_clusters)
+    knn_graph = kneighbors_graph(X, 20, include_self=False, n_jobs=-1)
+    predictor = AgglomerativeClustering(n_clusters=n_clusters,
+                                        connectivity=knn_graph,
+                                        linkage='ward',
+                                        affinity='euclidean')
     print(time.ctime(), 'Starting to fit.')
     labels = predictor.fit_predict(X)
-    print(time.ctime(), 'Cluster sizes:', len(np.bincount(labels)), np.bincount(labels))
-
-    indices_to_keep = remove_noise_clusters(X, labels)
-
-    labels = [labels[i] for i in indices_to_keep]
-    X = [X[i] for i in indices_to_keep]
-    likes = [likes[i] for i in indices_to_keep]
-    comments = [comments[i] for i in indices_to_keep]
-    shares = [shares[i] for i in indices_to_keep]
-    tf = [tf[i].toarray()[0].tolist() for i in indices_to_keep]
-    messages = [messages[i] for i in indices_to_keep]
-    dates = [dates[i] for i in indices_to_keep]
-
+    print(time.ctime(), 'Cluster sizes:', len(np.bincount(labels)),
+          np.bincount(labels))
 
     print(time.ctime(), 'Calinski harabaz score: {:.4f}'.format(calinski_harabaz_score(X, labels)))
     print(time.ctime(), 'Silhouette score:       {:.4f}'.format(silhouette_score(X, labels)))
@@ -496,7 +466,9 @@ def text_clustering(raw_data, pagename):
     plot_clusters(X, labels, pagename)
 
     print(time.ctime(), 'Printing.')
-    print_clusters(labels, likes, comments, shares, messages, tf, dates, vectorizer, orig_size, pagename, fan_count, page_name_displayed)
+    print_clusters(labels, likes, comments, shares, messages, tf, dates,
+                   vectorizer, orig_size, pagename, fan_count,
+                   page_name_displayed)
 
 
 def main():
